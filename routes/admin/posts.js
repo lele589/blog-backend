0a4ee442a6b06c2a0ffec6677998f7aee69b7e65
elenacarrasco <elenacfnb@gmail.com>
2021-04-06 14:45:55 +0200

    Update admin/posts/create route

--- routes/admin/posts.js
+++ routes/admin/posts.js
@@ -1,27 +1,37 @@
 const router = require('express').Router();
 const dayjs = require('dayjs');
+const Post = require('../../models/Post');
 
 // GET
 //# route => /admin/posts
-router.get('/', function(req, res, next) {
+router.get('/', function(req, res) {
     res.render('pages/posts/list', { username: req.cookies.username});
 });
 
 //# route => /admin/posts/new
-router.get('/new', function(req, res, next) {
-    const currentDate = dayjs().format('DD/MM/YYYY');
-    res.render('pages/posts/new', { currentDate });
+router.get('/new', function(req, res) {
+    res.render('pages/posts/new');
 });
 
 //# route => /admin/posts/edit/:idPost
-router.get('/edit/:idPost', function(req, res, next) {
+router.get('/edit/:idPost', function(req, res) {
     res.send('Form para editar un post --> ' + req.params.idPost);
 });
 
 // POST
 //# route => /admin/posts/create
-router.post('/create', function(req, res, next) {
-    res.send('Enviar datos del form para crear un post');
+router.post('/create', (req, res) => {
+    req.body.public === 'true' ? req.body.public = true : req.body.public = false
+    req.body.pets === 'on' ? req.body.pets = true : req.body.pets = false
+    req.body.date = dayjs();
+
+    Post.create(req.body)
+        .then(newPost => {
+            res.redirect('/admin/posts');
+        })
+        .catch(error => {
+            res.json({ error: error.message })
+        })
 });
 
 //# route => /admin/posts/update
