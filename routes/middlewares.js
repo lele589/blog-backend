f6654fc118fe162451a0dcb7c7fc3c3688faac84
elenacarrasco <elenacfnb@gmail.com>
2021-04-19 19:53:55 +0200

    Create FormValidation middleware

--- routes/middlewares.js
+++ routes/middlewares.js
@@ -1,6 +1,6 @@
 const jwt = require('jsonwebtoken');
 const dayjs = require('dayjs');
-const User = require('../models/User');
+const { check } = require('express-validator');
 
 const formNormalize = (req, res, next) => {
     req.body.public === 'true' ? req.body.public = true : req.body.public = false
@@ -10,6 +10,18 @@ const formNormalize = (req, res, next) => {
     next();
 }
 
+const formValidation = [
+    check('title', 'El campo "título" es obligatorio').exists().trim(),
+    check('category', 'El campo "categoría" es obligatorio').exists().trim(),
+    check('image', 'El campo "imagen" debe tener el formato correcto de URL').isURL().optional().trim(),
+    check('location', 'El campo "localización" es obligatorio y debe tener el formato correcto de URL').isURL().trim(),
+    check('price', 'El campo "precio" es obligatorio y debe ser un número entre 0 y 10000').isNumeric().trim(),
+    check('description', 'El campo "descripción" es obligatorio').exists().trim(),
+    check('pets', 'El campo "mascotas" es obligatorio y debe ser un buleano').isBoolean(),
+    check('public', 'El campo "publicar" es obligatorio y debe ser un buleano').isBoolean(),
+]
+
 module.exports = {
-    formNormalize
+    formNormalize,
+    formValidation
 }
