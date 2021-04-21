ae6a7b1b8aee7726ceb182fd56bbf257c81eaee3
elenacarrasco <elenacfnb@gmail.com>
2021-04-21 19:00:27 +0200

    Create API documentation (swagger)

--- app.js
+++ app.js
@@ -15,6 +15,9 @@ require('./dbConfig');
 
 const app = express();
 
+const swaggerUi = require('swagger-ui-express'),
+swaggerDocument = require('./swagger.json');
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
@@ -39,6 +42,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/admin', adminRouter);
 app.use('/api', apiRouter);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
