1ae9db35c8edb847d6eaf6ac2bcea3d37fec666f
elenacarrasco <elenacfnb@gmail.com>
2021-04-29 14:36:34 +0200

    Add cors dependency

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const cors = require('cors');
 const sassMiddleware = require('node-sass-middleware');
 
 require('dotenv').config();
@@ -21,6 +22,7 @@ swaggerDocument = require('./swagger.json');
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
